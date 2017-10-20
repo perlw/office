7746361a72d9f95cb2801204bf0ddd68b2797487
Per Lönn Wege <doxxan@gmail.com>
2017-10-20 13:46:44 +0200

    Silence gcc

--- src/scenes/sound-test.c
+++ src/scenes/sound-test.c
@@ -85,7 +85,8 @@ void scene_sound_test_stop(void **scene) {
   for (uint32_t t = 0; t < rectify_array_size(ptr->sounds); t++) {
     free(ptr->sounds[t]);
   }
-  rectify_array_free(&(void *)ptr->sounds);
+  void *sounds = (void *)ptr->sounds;
+  rectify_array_free(&sounds);
 
   free(ptr);
   *scene = NULL;
